14913f573a8c1209fdb57944301224932388546e
davidbrocardo@gmail.com <70301267+DavidBrocardo@users.noreply.github.com>
2024-07-29 13:37:32 -0300

    Automato finalizado

--- lexico.py
+++ lexico.py
@@ -1,4 +1,4 @@
-alphabet_low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"] # Inclui _ nos 2 alfabetos maiusculos e minusculos
+alphabet_low = ["_","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "_"] # Inclui _ nos 2 alfabetos maiusculos e minusculos
 
 alphabet_high = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"]
 
@@ -9,12 +9,17 @@ op_arit = ["*", "+", "-", "/"]
 class Automato:
     def __init__(self):
         self.tokens = []
+        self.code_enter = []
         self.code = []
         
     def read_file(self, file_name):
         with open(file_name, "r") as reading:
-            self.code = reading.readlines()
-        print(self.code)
+            self.code_enter = reading.readlines()
+            i=0
+            for self.code_enter in self.code_enter:
+                self.code.insert(i,self.code_enter.split())
+                i += 1
+            print(self.code)
 
     def analyzes_code(self):
         for string in self.code:
@@ -24,65 +29,141 @@ class Automato:
         state = 1
 
         for letter in string:
+            #letter_space = ' '.join([char for char in letter]) Fui burro nao precisa disso
+            print(letter)
+            for carac in letter:
+                print(carac)
+                print(state)
+                if state == 1: # Para o estado 1 do automato, fazer a regra de todas as transicoes
+                    if carac in alphabet_low or carac in alphabet_high:
+                        state = 7
 
-            if state == 1: # Para o estado 1 do automato, fazer a regra de todas as transicoes
-                if letter in alphabet_low or letter in alphabet_high:
-                    state = 7
+                    elif carac in numbers:
+                        state = 2
 
-                elif letter in numbers:
-                    state = 2
+                    elif carac == '"':
+                        state = 9
 
-                elif letter == '"':
-                    state = 9
+                    elif carac == '(':
+                        state = 1
+                        self.tokens.extend([carac, "par_esq"])
 
-                elif letter == '(':
-                    state = 1
-                    self.tokens.append(letter, "par_esq")
+                    elif carac == ')':
+                        state = 1
+                        self.tokens.extend([carac, "par_dir"])
 
-                elif letter == ')':
-                    state = 1
-                    self.tokens.append(letter, "par_dir")
+                    elif carac == '{':
+                        print("entrou")
+                        state = 1
+                        self.tokens.extend([carac, "col_esq"])
 
-                elif letter == '{':
-                    state = 1
-                    self.tokens.append(letter, "col_esq")
+                    elif carac == '}':
+                        state = 1
+                        self.tokens.extend([carac, "col_dir"])
 
-                elif letter == '}':
-                    state = 1
-                    self.tokens.append(letter, "col_dir")
+                    elif carac == "@":
+                        state = 13
 
-                elif letter == "@":
-                    state = 13
+                    elif carac == ';':
+                        state = 15
 
-                elif letter == ';':
-                    state = 15
+                    elif carac in op_arit:
+                        state = 1
+                        self.tokens.extend([letter, "op_aritmetico"])
 
-                elif letter in op_arit:
-                    state = 1
-                    self.tokens.append(letter, "op_aritmetico")
+                    elif carac in ["<", ">"]:
+                        state = 18    
 
-                elif letter in ["!", "<", ">"]:# Tem que ver como fica
-                    state = 1
-                    self.tokens.append(string, "op_relacional")
+                    elif carac in ["!"]:
+                        state = 19
 
-                elif letter == ":":
-                    state = 19
+                    elif carac == "=":
+                        state = 1
+                        self.tokens.extend([letter, "op_relacional"])
 
-                elif letter == ",":
-                    state = 1
-                    self.tokens.append(letter, "virgula")
+                    elif carac == ":":
+                        state = 23
 
-                else:
-                    print("Erro no estado ", state)
+                    elif carac == ",":
+                        state = 1
+                        self.tokens.extend([letter, "virgula"])
+                    else:
+                        print("Erro no estado ", state)
 
-            elif state == 19:
-                if letter == "=":
-                    state = 1
-                    self.tokens.append(":=", "atribuicao")
-        
+    
+                elif state == 2:
+                    if carac in numbers:
+                        state = 2
+                    elif carac in ".":
+                        state = 4
+                    else :
+                        state = 1
+                        self.tokens.extend([letter, "int"])
+
+                elif state == 4:
+                    if carac in numbers:
+                        state = 5
+
+                elif state == 5:
+                    if carac in numbers:
+                        state = 5
+                    else:
+                        state = 1
+                        self.tokens.extend([letter, "float"])
+                               
+                elif state == 7:
+                    if carac in numbers:
+                        state = 7
+                    elif carac in alphabet_low or carac in alphabet_high:
+                        state = 7
+                    else:
+                        state = 1 
+                        self.tokens.extend([letter, "id"])
+
+                elif state == 9:
+                    if carac in '"':
+                        state = 1
+                        self.tokens.extend([letter, "string"])
+                    else:
+                        state = 9
+
+                elif state == 13:
+                    if carac in "\n":
+                        state = 1
+                        self.tokens.extend([letter, "comentario"])
+                    else:
+                        state = 13
+
+                elif state == 15:
+                    if carac in "\n":
+                        state = 1
+                        self.tokens.extend([letter, "fim_linha"])
+                    else:
+                        print("Erro 15")
+
+                elif state == 18:
+                    if carac == "=":
+                        state = 1
+                        self.tokens.extend([letter, "op_relacional"])
+                    else:
+                        state = 1
+                        self.tokens.extend([letter, "op_relacional"])
+
+                elif state == 19:
+                    if carac == "=":
+                        state = 1
+                        self.tokens.extend([letter, "atribuicao"])
+
+                elif state == 23:
+                    if carac == "=":
+                        state = 1
+                        self.tokens.extend([letter, "atribuicao"])
+
                 else:
                     print("Erro no estado ", state)
 
 my_lex = Automato()  
 
 my_lex.read_file("cod_teste.txt")
+my_lex.analyzes_code()
+print(my_lex.tokens)
