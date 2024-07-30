7825e5aec77e5817ac279e6ba6fb3eb305df6f88
TadiotoG <tadiotogabriel@hotmail.com>
2024-07-30 10:44:37 -0300

    Lexico testado

--- lexico.py
+++ lexico.py
@@ -15,17 +15,21 @@ class Automato:
     def read_file(self, file_name):
         with open(file_name, "r") as reading:
             self.code_enter = reading.readlines()
-            #i=0
-            print(self.code_enter)
-            #for self.code_enter in self.code_enter:
+            # i=0
+            # print(self.code_enter)
+            # for self.code_enter in self.code_enter:
             #    self.code.insert(i,self.code_enter.split())
             #    i += 1
             
 
     def analyzes_code(self):
         for string in self.code_enter:
-            print(string)
+            # print(string)
             self.automato_rules(string)
+
+    def print_tokens(self):
+        for token in self.tokens:
+            print("Código encontrado: ", token[0], "    Token: ", token[1], "\n")
 
     def automato_rules(self, string):
         state = 1
@@ -33,9 +37,9 @@ class Automato:
         controle = 0
         while i < len(string):
             carac = string[i]
-            print("Carac:" ,carac, "State:" ,state, "Indice :" ,i)
-            print(carac)
-            print(state)
+            # print("Carac:" ,carac, "State:" ,state, "Indice :" ,i)
+            # print(carac)
+            # print(state)
             if state == 1: # Para o estado 1 do automato, fazer a regra de todas as transicoes
                 if carac in alphabet_low or carac in alphabet_high:
                     self.txt =  carac
@@ -51,19 +55,23 @@ class Automato:
 
                 elif carac == '(':
                     state = 1
-                    self.tokens.extend([carac, "par_esq"])
+                    self.tokens.append([carac, "par_esq"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
 
                 elif carac == ')':
                     state = 1
-                    self.tokens.extend([carac, "par_dir"])
+                    self.tokens.append([carac, "par_dir"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
 
                 elif carac == '{':
                     state = 1
-                    self.tokens.extend([carac, "col_esq"])
+                    self.tokens.append([carac, "col_esq"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
 
                 elif carac == '}':
                     state = 1
-                    self.tokens.extend([carac, "col_dir"])
+                    self.tokens.append([carac, "col_dir"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
 
                 elif carac == "@":
                     self.txt =  carac
@@ -76,7 +84,8 @@ class Automato:
                 elif carac in op_arit:
                     self.txt =  carac
                     state = 1
-                    self.tokens.extend([carac, "op_aritmetico"])
+                    self.tokens.append([carac, "op_aritmetico"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
 
                 elif carac in ["<", ">"]:
                     self.txt =  carac
@@ -88,7 +97,8 @@ class Automato:
 
                 elif carac == "=":
                     state = 1
-                    self.tokens.extend([carac, "op_relacional"])
+                    self.tokens.append([carac, "op_relacional"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
 
                 elif carac == ":":
                     self.txt =  carac
@@ -96,10 +106,14 @@ class Automato:
 
                 elif carac == ",":
                     state = 1
-                    self.tokens.extend([carac, "virgula"])
+                    self.tokens.append([carac, "virgula"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
+
                 else:
-                    if carac != " " :
-                        print("Erro no estado ", state)
+                    if carac != " " and carac != "\n":
+                        self.txt +=  carac
+                        print("Erro no estado ", state, "  caracter:", self.txt, "\n")
+                        state = 1
 
 
             elif state == 2:
@@ -109,10 +123,17 @@ class Automato:
                 elif carac in ".":
                     self.txt +=  carac
                     state = 4
-                else :
+
+                elif carac in alphabet_low or carac in alphabet_high:
+                    self.txt +=  carac
+                    print("Erro no estado ", state, "  caracter:", self.txt, "\n")
+                    state = 1
+                    
+                else:
                     controle = 1
                     state = 1
-                    self.tokens.extend([self.txt, "int"])
+                    self.tokens.append([self.txt, "int"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
 
             elif state == 4:
@@ -124,10 +145,18 @@ class Automato:
                 if carac in numbers:
                     self.txt +=  carac
                     state = 5
+
+                elif carac in alphabet_low or carac in alphabet_high:
+                    self.txt +=  carac
+                    print("Erro no estado ", state, "  caracter:", self.txt, "\n")
+                    state = 1
+                    controle = 1
+
                 else:
                     controle = 1
                     state = 1
-                    self.tokens.extend([self.txt, "float"])
+                    self.tokens.append([self.txt, "float"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
                             
             elif state == 7:
@@ -140,14 +169,16 @@ class Automato:
                 else:
                     controle = 1
                     state = 1 
-                    self.tokens.extend([self.txt, "id"])
+                    self.tokens.append([self.txt, "id"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
 
             elif state == 9:
                 if carac in '"':
                     self.txt +=  carac
                     state = 1
-                    self.tokens.extend([self.txt, "string"])
+                    self.tokens.append([self.txt, "string"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
                 else:
                     self.txt +=  carac
@@ -157,7 +188,8 @@ class Automato:
                 if carac in '\n':
                     state = 1
                     self.txt +=  carac
-                    self.tokens.extend([self.txt, "comentario"])
+                    self.tokens.append([self.txt, "comentario"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
                 else:
                     self.txt +=  carac
@@ -167,44 +199,65 @@ class Automato:
                 if carac in "\n":
                     state = 1
                     self.txt +=  carac
-                    self.tokens.extend([self.txt, "fim_linha"])
+                    self.tokens.append([self.txt, "fim_linha"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
+
+                else:
+                    self.txt +=  carac
+                    print("Erro no estado ", state, "  caracter:", self.txt, "\n")
+                    state = 1
 
             elif state == 18:
                 if carac == "=":
                     self.txt +=  carac
                     state = 1
-                    self.tokens.extend([self.txt, "op_relacional"])
+                    self.tokens.append([self.txt, "op_relacional"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
                 else:
                     controle = 1
                     state = 1
-                    self.tokens.extend([self.txt, "op_relacional"])
+                    self.tokens.append([self.txt, "op_relacional"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
 
             elif state == 19:
                 if carac == "=":
                     self.txt +=  carac
                     state = 1
-                    self.tokens.extend([self.txt, "op_relacional"])
+                    self.tokens.append([self.txt, "op_relacional"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
+
+                else:
+                    self.txt +=  carac
+                    print("Erro no estado ", state, "  caracter:", self.txt, "\n")
+                    state = 1
+                    controle = 1
 
             elif state == 23:
                 if carac == "=":
                     self.txt +=  carac
                     state = 1
-                    self.tokens.extend([self.txt, "atribuicao"])
+                    self.tokens.append([self.txt, "atribuicao"])
+                    print("Código encontrado: ", self.tokens[-1][0], "    Token: ", self.tokens[-1][1], "\n")
                     self.txt = ""
 
-            else:
-                print("Erro no estado ", state)
-            
+                else:
+                    self.txt +=  carac
+                    print("Erro no estado ", state, "  caracter:", self.txt, "\n")
+                    state = 1
+                    controle = 1
+                
             if controle == 0:
                 i += 1  # Incrementar o índice para a próxima iteração
             else:
                 controle = 0
 my_lex = Automato()  
 
-my_lex.read_file("cod_teste.txt")
+my_lex.read_file("cod_teste3.txt")
 my_lex.analyzes_code()
+
+# my_lex.print_tokens()
 print(my_lex.tokens)
