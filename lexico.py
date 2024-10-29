7a6e18dfea4da7285e87e0c5a960e569f377723a
davidbrocardo@gmail.com <70301267+DavidBrocardo@users.noreply.github.com>
2024-10-29 10:07:29 -0300

    Sla oque eu fiz

--- lexico.py
+++ lexico.py
@@ -812,7 +812,7 @@ class Automato:
                 controle = 0
 my_lex = Automato()  
 
-my_lex.read_file("cod_teste3.txt")
+my_lex.read_file("exemplo.txt")
 my_lex.analyzes_code()
 #print(my_lex.code_enter)
 
